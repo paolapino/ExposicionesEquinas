21317c246dc24671058d34dbcd19529beffe99a8
Willinton <wjaniervalvarez@misena.edu.co>
2019-04-28 16:56:36 -0500

    cuarta subida

--- src/layout/MainNavbar.vue
+++ src/layout/MainNavbar.vue
@@ -40,21 +40,21 @@
                         data-toggle="dropdown"
                       >
                         <i class="material-icons">apps</i>
-                        <p>Components</p>
+                        <p>Otros LInks??</p>
                       </md-button>
                       <ul class="dropdown-menu dropdown-with-icons">
                         <li>
                           <a href="#/">
                             <i class="material-icons">layers</i>
-                            <p>All Components</p>
+                            <p>?????</p>
                           </a>
                         </li>
                         <li>
                           <a
-                            href="https://demos.creative-tim.com/vue-material-kit/documentation/"
+                            href="#"
                           >
                             <i class="material-icons">content_paste</i>
-                            <p>Documentation</p>
+                            <p>??????</p>
                           </a>
                         </li>
                       </ul>
@@ -77,7 +77,23 @@
                 v-if="showDownload"
               >
                 <i class="material-icons">cloud_download</i>
-                <p>Download</p>
+                <p>Otro enlace??</p>
+              </md-list-item>
+              <md-list-item
+                href="#/profile"
+                @click="scrollToElement()"
+                v-if="showDownload"
+              >
+                <i class="material-icons">cloud_download</i>
+                <p>(Prueba)Perfil Usuario</p>
+              </md-list-item>
+              <md-list-item
+                href="#/profileAdmi"
+                @click="scrollToElement()"
+                v-if="showDownload"
+              >
+                <i class="material-icons">cloud_download</i>
+                <p>(Prueba) perfil Administrador</p>
               </md-list-item>
 
               <li class="md-list-item" v-else>
@@ -85,38 +101,7 @@
                   href="javascript:void(0)"
                   class="md-list-item-router md-list-item-container md-button-clean dropdown"
                 >
-                  <div class="md-list-item-content">
-                    <drop-down direction="down">
-                      <md-button
-                        slot="title"
-                        class="md-button md-button-link md-white md-simple dropdown-toggle"
-                        data-toggle="dropdown"
-                      >
-                        <i class="material-icons">view_carousel</i>
-                        <p>Examples</p>
-                      </md-button>
-                      <ul class="dropdown-menu dropdown-with-icons">
-                        <li>
-                          <a href="#/landing">
-                            <i class="material-icons">view_day</i>
-                            <p>Landing Page</p>
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#/login">
-                            <i class="material-icons">fingerprint</i>
-                            <p>Login Page</p>
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#/profile">
-                            <i class="material-icons">account_circle</i>
-                            <p>Profile Page</p>
-                          </a>
-                        </li>
-                      </ul>
-                    </drop-down>
-                  </div>
+                  
                 </a>
               </li>
 
@@ -206,7 +191,7 @@ export default {
   },
   computed: {
     showDownload() {
-      const excludedRoutes = ["login", "landing", "profile"];
+      const excludedRoutes = ["profileAdmi","landing", "profile"];
       return excludedRoutes.every(r => r !== this.$route.name);
     }
   },
